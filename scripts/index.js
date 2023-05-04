1c0f98f3cbeff7918a985fc5d58e38047ac3fbaf
Cody O'Brien <cody@cdso.dev>
2023-05-03 22:50:53 -0400

    add functionality to close modal when clicking outside

--- scripts/index.js
+++ scripts/index.js
@@ -29,6 +29,15 @@ const cardTemplateContent = document.querySelector("#card").content;
 const cardList = document.querySelector(".cards__list");
 const modalPicture = document.querySelector(".modal-picture");
 
+const modalList = Array.from(document.querySelectorAll(".modal"));
+modalList.forEach((modal) => {
+  modal.addEventListener("click", (evt) => {
+    if (evt.target.classList.contains("modal")) {
+      closeModal(modal);
+    }
+  });
+});
+
 initialCards.forEach((element) => {
   cardList.append(getCardElement(element));
 });
